41da3fb099513c111e449fc444867f710092aca1
pagidipalaravi <pagidipala.ravi@gmail.com>
2022-07-08 08:20:39 +0530

    application raw components
    application raw components

---
+++ src/app/navigation/lhs-navigation/lhs-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lhs-navigation',
+  templateUrl: './lhs-navigation.component.html',
+  styleUrls: ['./lhs-navigation.component.css']
+})
+export class LhsNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
