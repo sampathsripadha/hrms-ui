100b6cc34680437079e57b53856b50b136806862
pohem <poshemprashanth@gmail.com>
2022-07-08 08:04:41 +0530

    View employee creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewemployeeComponent } from './viewemployee/viewemployee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewemployeeComponent
   ],
   imports: [
     BrowserModule,
